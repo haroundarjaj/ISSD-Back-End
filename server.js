a89f875d48c3cacaeef88fdd21e5b330d6fcd913
Haroun Darjaj <darjaj.haroun@gmail.com>
2022-06-13 11:59:56 +0100

    fix

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // simple route
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to bezkoder application." });
+    res.json({ message: "Welcome to ISSD application." });
 });
 require("./app/routes/address.routes.js")(app);
 // set port, listen for requests
