66b433d5c9e26823f915c375b84c714ebf25c75c
Haroun <darjaj.haroun@gmail.com>
2022-06-13 12:15:35 +0100

    db fix

--- app/models/db.js
+++ app/models/db.js
@@ -1,15 +1,28 @@
-const mysql = require("mysql");
+var mysql = require('mysql')
 const dbConfig = require("../config/db.config.js");
-// Create a connection to the database
-const connection = mysql.createConnection({
+
+var pool = mysql.createPool({
+    connectionLimit: 10,
     host: dbConfig.HOST,
     user: dbConfig.USER,
     password: dbConfig.PASSWORD,
     database: dbConfig.DB
-});
-// open the MySQL connection
-connection.connect(error => {
-    if (error) throw error;
+})
+pool.getConnection((err, connection) => {
+    if (err) {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            console.error('Database connection was closed.')
+        }
+        if (err.code === 'ER_CON_COUNT_ERROR') {
+            console.error('Database has too many connections.')
+        }
+        if (err.code === 'ECONNREFUSED') {
+            console.error('Database connection was refused.')
+        }
+    }
+    if (connection) connection.release()
     console.log("Successfully connected to the database.");
-});
-module.exports = connection;
+    return
+})
+module.exports = pool
+
