46d3568622e054f15321c3ac93516890d1f267cb
cesarjacome97 <107264076+cesarjacome97@users.noreply.github.com>
2022-07-13 09:11:28 -0500

    Updated with better practices

--- app/controllers/address.controller.js
+++ app/controllers/address.controller.js
@@ -26,3 +26,59 @@ exports.getById = (req, res) => {
         } else res.send(data);
     });
 };
+//indeterminate an address
+exports.indetermina = (req, res) => {
+    model.indetermina(req.params.id, (err, data) => {
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).send({
+                    message: `Not found data with id ${req.params.id}.`
+                });
+            } else {
+                res.status(500).send({
+                    message: "Error retrieving data with id " + req.params.id
+                });
+            }
+        } else res.send(data);
+    });
+};
+//get xml for maped data of database
+exports.getXml = (req, res) => {
+    model.getXml((err, data) => {
+        if (err)
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving data."
+            });
+        else res.send(data);
+    });
+};
+//get xml for maped data of database
+exports.getXmlapi = (req, res) => {
+    model.getXmlapi((err, data) => {
+        if (err)
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving data."
+            });
+        else res.send(data);
+    });
+};
+
+//update an address
+exports.actualiza = (req, res) => {
+	//req.params.id
+    model.actualiza(req, (err, data) => {
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).send({
+                    message: `Not found data with id ${req.params.id}.`
+                });
+            } else {
+                res.status(500).send({
+                    message: "Error retrieving data with id " + req.params.id
+                });
+            }
+        } else res.send(data);
+    });
+};
